852703e344e6ee2e051a7cd6db28a3040ff88594
charaneie22 <charaneie22@gmail.com>
2018-05-10 21:12:56 +0530

    Initial commiit

---
+++ app/components/MapItem.js
@@ -0,0 +1,55 @@
+// import React, { Component } from 'react'
+// import { Text, StyleSheet, View } from 'react-native'
+// import MapView from 'react-native-maps';
+// export default class MapItem extends Component {
+//   render() {
+//     return (
+//       <View>
+//         <Text> textInComponent </Text>
+        
+//       </View>
+//     )
+//   }
+// }
+
+// const styles = StyleSheet.create({})
+import React, { Component } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Constants, MapView } from 'expo';
+
+export default class MapItem extends Component {
+  state = {
+    mapRegion: {
+      latitude: 37.78825,
+      longitude: -122.4324,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    },
+  };
+
+  _handleMapRegionChange = mapRegion => {
+    this.setState({ mapRegion });
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <MapView
+          style={{ alignSelf: 'stretch', height: 200 }}
+          region={this.state.mapRegion}
+          onRegionChange={this._handleMapRegionChange}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+  },
+});
